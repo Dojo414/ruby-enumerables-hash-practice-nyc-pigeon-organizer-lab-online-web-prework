0fba949b44ed98986280e31654c2c2dcc924f1b6
Donovan Jones <donovanljones94@gmail.com>
2019-07-02 23:56:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,18 @@ def nyc_pigeon_organizer(data)
   sorted_pigeons = {}
   
   data.each do |first_point, bird_color|
-    bird_color.each do |
+    bird_color.each do |color, name|
+      name.each do |bird_name|
+        sorted_pigeons[bird_name] = {:color => [], :gender => [], :lives[]}
+      end
+    end
+  end
+  
+  birdies = sorted_pigeons.keys
+  
+  data.each do |first_point, bird_color|
+    bird_color.each do |color, name|
+      birdies.each do |names|
+        if bird_color[name] === birdies[names]
+          sorted_pigeons[names][:color] = 
 end
