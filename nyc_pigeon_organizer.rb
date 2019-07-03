8ef466588308ccee78a9320f1c53f4dc13ebdadc
Donovan Jones <donovanljones94@gmail.com>
2019-07-03 00:12:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,18 @@
 
 
 def nyc_pigeon_organizer (data)
+organized_pigeons = {}
+
+
+data.each do |first_point, next_level|
+  next_level.each do |color, bird_name|
+    bird_name.each do |name|
+      organized_pigeons[name] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+end
 
 
 
-
-
+return organized_pigeons
 end 
