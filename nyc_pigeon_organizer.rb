6aa51fbdd2515a685ec6ff5a9b34f35e256e3439
Donovan Jones <donovanljones94@gmail.com>
2019-07-03 00:25:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,7 @@ birdies = organized_pigeons.keys
 # Place the colors into the new hash
 
 data[:color].each do |upper_level, color|
-  color.each do |colors, name|
+  color.each do |name|
     birdies.each do |object|
       if object === name
         organized_pigeons[object][:color] << upper_level.to_s
@@ -30,3 +30,44 @@ return organized_pigeons
 end 
 
 
+def nyc_pigeon_organizer (data)
+  final = {}
+
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
+  return final 
+end 
